df80b977fc10959183524216eff482b7475919cd
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-04-30 19:17:24 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port =process.env.PORT || 5000;
 
 const categories = require('./data/categories.json')
 
+const news = require('./data/news.json')
+
 app.use(cors())
 
 app.get('/',(req, res)=>{
@@ -13,6 +15,27 @@ app.get('/',(req, res)=>{
 
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+
+app.get('/news', (req, res)=>{
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    if(id === 0){
+        res.send(news)
+    }
+    else{
+        const categoryNews = news.filter(n=>parseInt(n.category_id) === id);
+        res.send(categoryNews)
+    }
 })
 
 app.listen(port, ()=>{
